d5e1cb401a5e36cdade82462eafb35abfb6af988
Kevin Shi <xpspectre@gmail.com>
2019-04-15 17:58:55 -0400

    .bat file comments Yes, it's not #

--- ci/build_windows.bat
+++ ci/build_windows.bat
@@ -1,28 +1,28 @@
-# Setup environment, test, build, and package
+:: Setup environment, test, build, and package
 
-# Create virtualenv for project in root dir
+:: Create virtualenv for project in root dir
 python -m venv venv
 
-# Activate env
+:: Activate env
 venv\Scripts\activate.bat
 
-# Show python version check
+:: Show python version check
 python  --version
 
-# Install wheel, which is may be missing from virtualenv
+:: Install wheel, which is may be missing from virtualenv
 pip install wheel
 
-# Install requirements
+:: Install requirements
 pip install -r requirements.txt
 
-# Run tests in tests/ dir
+:: Run tests in tests\ dir
 python -m unittest
 
-# Make PyPI package
+:: Make PyPI package
 python setup.py bdist_wheel
 
-# Build Windows app
+:: Build Windows app
 pyinstaller --clean -y test_ci\run.py
 
-# Package
+:: Package
 7z a dist\run_test_ci.zip .\test_ci\dist\run\*
